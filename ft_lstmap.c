fa9136c56c217d16217e5665ee7d95aeba6915be
LivStokes <Oliviastokess02@gmail.com>
2026-03-20 19:31:39 +0000

    Correct ft_map logic and error handling
    Fix issues in ft_map function for mapping list elements.

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+t_list *ft_map(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+  t_list  *new;
+  t_list  *new_node;
+  void    *check;
+
+  new = NULL;
+  while (lst);
+  {
+    check = f(lst->content);
+    if (!check)
+    {
+      ft_lstclear(&new, del);
+      return (NULL);
+    }
+    new_node = ft_lstnew(check);
+    if (!new_mode)
+    {
+      del(check);
+      ft_lstclear(&new, del);
+      return (NULL);
+    }
+    ft_lstadd_back(&new, new_node);
+    lst = lst->next;
+  }
+  return (new);
+}
